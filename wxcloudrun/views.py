8c5e01d209efce11c85685f57b0051614e87c322
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-03-30 22:12:25 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -15,15 +15,15 @@ import requests
 
 def duixiangcunchu():
   #获取token
-  response = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxfb2997f507abf89e&secret=168726b557fb7221c96955cec59b3347',)
+  response = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxfb2997f507abf89e&secret=168726b557fb7221c96955cec59b3347',verify=False)
   data ={
     "env": "prod-0gayxkvve034fe60",
     "path": "ciyunimage/ciyun.jpg"
   }
-  return str(response.json())
+  #return str(response.json())
   #转json
   data = json.dumps(data)
-  response = requests.post("https://api.weixin.qq.com/tcb/uploadfile?access_token="+response.json()['access_token'],data)
+  response = requests.post("https://api.weixin.qq.com/tcb/uploadfile?access_token="+response.json()['access_token'],data,verify=False)
   print(response.json())
   #response = json.loads(response.json())
   #得到上传链接
@@ -37,7 +37,7 @@ def duixiangcunchu():
     'file': ('ciyun.jpg',open(r"/app/wxcloudrun/ciyun.jpg","rb"))
   }
   #data2 = json.dumps(data2)
-  response2 = requests.post(response.json()['url'], files=data2)
+  response2 = requests.post(response.json()['url'], files=data2,verify=False)
   return response.json()["file_id"]
 
 @app.route('/', methods=['POST'])

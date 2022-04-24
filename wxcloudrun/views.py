dd4b9ad854720a6c91409fff03fc8a6c08b05966
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-04-24 22:03:46 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,4 +1,3 @@
-#-*- coding: utf-8 -*-
 
 from datetime import datetime
 from flask import render_template, request,Flask
@@ -9,15 +8,20 @@ from wxcloudrun.response import make_succ_empty_response, make_succ_response, ma
 import pkuseg
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import matplotlib
 from imageio import imread
 import  os
 import jieba
 import json
 import requests
 
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
+
+# 指定默认字体
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+matplotlib.rcParams['font.family'] = 'sans-serif'
+# 解决负号'-'显示为方块的问题
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 def duixiangcunchu(courseid):
   #获取token
@@ -53,18 +57,18 @@ def upload():
     json_data = file
     plt.figure(figsize=(10,10))#将画布设定为正方形，则绘制的饼图是正圆
     fig1 = plt.subplot(121)
-    label=[u'完全明白',u'一般般',u'不大好',u'完全听不懂']#定义饼图的标签，标签是列表
+    label=['完全明白','一般般','不大好','完全听不懂']#定义饼图的标签，标签是列表
     explode=[0.01, 0.01, 0.01, 0.01]#设定各项距离圆心n个半径
     values=[json_data['完全明白'], json_data['一般般'], json_data['不大好'], json_data['完全听不懂']]
     plt.pie(values,explode=explode,labels=label,autopct='%1.1f%%')#绘制饼图
-    plt.title(u'本次课程学生投票情况1')
+    plt.title('本次课程学生投票情况1')
 
     fig2 = plt.subplot(122)
-    label=[u'踩', u'赞']#定义饼图的标签，标签是列表
+    label=['踩', '赞']#定义饼图的标签，标签是列表
     explode=[0.01, 0.01]#设定各项距离圆心n个半径
     values=[json_data['踩'], json_data['赞']]
     plt.pie(values,explode=explode,labels=label,autopct='%3.1f%%')#绘制饼图
-    plt.title(u'本次课程学生投票情况2')
+    plt.title('本次课程学生投票情况2')
     plt.savefig('/app/wxcloudrun/visualization.jpg')
     a = duixiangcunchu(courseid)
     b = {"re":a}

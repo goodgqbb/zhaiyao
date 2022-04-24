35db146389d4275d937138657912437f62422e7f
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-04-24 21:19:35 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,3 +1,8 @@
+#-*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
 from datetime import datetime
 from flask import render_template, request,Flask
 from run import app

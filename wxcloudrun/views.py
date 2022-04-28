6a82a73ec0c5aa06c2eac637417f65993578802f
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-04-28 12:22:24 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -11,63 +11,40 @@ from imageio import imread
 import  os
 import jieba
 import json
-import requests
+from flask import Flask, request, make_response
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.nlp.v20190408 import nlp_client, models
 
-def duixiangcunchu(courseid):
-  #获取token
-  response = requests.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxfb2997f507abf89e&secret=168726b557fb7221c96955cec59b3347',verify=False)
-  data ={
-    "env": "prod-0gayxkvve034fe60",
-    "path": "filecontent/"+courseid+".jpg"
-  }
-  #return str(response.json())
-  #转json
-  data = json.dumps(data)
-  response = requests.post("https://api.weixin.qq.com/tcb/uploadfile?access_token="+response.json()['access_token'],data,verify=False)
-  print(response.json())
-  #response = json.loads(response.json())
-  #得到上传链接
-
-  data2={
-    "Content-Type":(None,".jpg"),
-    "key": (None,"filecontent/"+courseid+".jpg"),
-    "Signature": (None,response.json()['authorization']),
-    'x-cos-security-token': (None,response.json()['token']),
-    'x-cos-meta-fileid': (None,response.json()['cos_file_id']),
-    'file': ('filecontent.jpg',open(r"/app/wxcloudrun/filecontent.jpg","rb"))
-  }
-  #data2 = json.dumps(data2)
-  response2 = requests.post(response.json()['url'], files=data2,verify=False)
-  return response.json()["file_id"]
 
 @app.route('/', methods=['POST'])
 def upload():
-#     stopwords={'我们','你们','他们','它们','因为','因而','所以','如果','那么',
-#           '如此','只是','但是','就是','这是','那是','而是','而且','虽然',
-#           '这些','有些','然后','已经','于是','一种','一个','一样','时候',
-#     '没有','什么','这样','这种','这里','不会','一些','这个','仍然','不是',
-#     '自己','知道','可以','看到','那儿','问题','一会儿','一点','现在','两个',
-#          '三个','我','的','是','以及','干嘛','用来','很','到底'}
-    num = ['1','2','3','4','5','6','7','8','9']
-    stopwords = set()
-    content = [line.strip() for line in open(r'/app/wxcloudrun/baidu_stopwords.txt','r',encoding="utf-8").readlines()]
-    stopwords.update(content)
-    stopwords.update(num)
-    texts = []
+    
+    text = request.json.get('text')
+    try:
+        cred = credential.Credential("AKIDIRN5arujJhrGTTGpBKFwOceTJxNaaMwI", "B792kHagYCg2e4LSN7h3A2jEUWKyOaXG")
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "nlp.tencentcloudapi.com"
 
-    file = request.json.get('allcomment')
-    courseid = request.json.get('courseid')
-    text = jieba.lcut(file)
-    for item in text:
-        if item not in stopwords:
-            texts.append(item)
-    text = str(texts)
-    bg_pic = imread('/app/wxcloudrun/R-C.jpg')
-    wordcloud = WordCloud(mask=bg_pic,background_color='white',font_path='/app/wxcloudrun/华文楷体.ttf',scale=1.5).generate(text)
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        client = nlp_client.NlpClient(cred, "ap-guangzhou", clientProfile)
 
-    wordcloud.to_file('/app/wxcloudrun/filecontent.jpg')
+        req = models.SentimentAnalysisRequest()
+        params = {
+            "Text": text,
+            "Mode": "2class"
+        }
+        req.from_json_string(json.dumps(params))
 
-    a = duixiangcunchu(courseid)
-    b = {"re":a}
-    c = json.dumps(b)
-    return c
+        resp = client.SentimentAnalysis(req)
+        if 'positive' in resp.to_json_string():
+            return json.dumps(1, ensure_ascii=False)
+        else:
+            return json.dumps(0, ensure_ascii=False)
+
+    except TencentCloudSDKException as err:
+        print(err)
+        return json.dumps('error!!!!', ensure_ascii=False)
